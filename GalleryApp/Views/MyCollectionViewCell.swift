5754258bacde8440205bc8c3bc103b7246f8de3a
Daria Gavrilova <dashkagavrilova024@gmail.com>
2025-09-07 06:43:12 -0700

    Added basic cell class

---
+++ GalleryApp/Views/MyCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MyCollectionViewCell.swift
+//  GalleryApp
+//
+//  Created by MAC on 07.09.25.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
