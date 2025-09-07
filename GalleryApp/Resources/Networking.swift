9305d3c1b2213288a7b73eb8cb19ab0c5fd7c2ae
Daria Gavrilova <dashkagavrilova024@gmail.com>
2025-09-07 12:05:48 -0700

    Added Link enum for building API URLs

---
+++ GalleryApp/Resources/Networking.swift
@@ -0,0 +1,19 @@
+//
+//  Networking.swift
+//  GalleryApp
+//
+//  Created by MAC on 07.09.25.
+//
+
+import Foundation
+
+enum Link {
+    case images(page: Int, limit: Int)
+    
+    var url: URL {
+        switch self {
+        case .images(let page, let limit):
+            return URL(string: "https://api.unsplash.com/photos?page=\(page)&per_page=\(limit)")!
+        }
+    }
+}
