0b0ecd6dd2b33dadf202fec82d14534115bb9469
Daria Gavrilova <dashkagavrilova024@gmail.com>
2025-09-07 12:13:12 -0700

    Implemented fetchPhoto method in NetworkManager

--- GalleryApp/Resources/Networking.swift
+++ GalleryApp/Resources/Networking.swift
@@ -23,4 +23,32 @@ final class NetworkManager: ObservableObject {
     init() {}
     
     static let shared = NetworkManager()
+    
+    var images = [Photo]()
+    private var token = "ttjV4npqziwmjqehd1Ibx3KVUgFWCA7eNxotJwfTOwQ"
+    
+    func fetchPhoto(page: Int = 1, limit: Int = 30, completion: @escaping ([Photo]) -> ()) {
+        var request = URLRequest(url: Link.images(page: page, limit: limit).url)
+        
+        request.httpMethod = "GET"
+        request.setValue("Client-ID \(token)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) {data, response, error in
+            guard let data = data, error == nil else { return }
+            
+            let httpsResponse = response as? HTTPURLResponse
+            print("Status code: \(String(describing: httpsResponse?.statusCode))")
+            
+            do {
+                let images = try JSONDecoder().decode([Photo].self, from: data)
+                DispatchQueue.main.async {
+                    completion(images)
+                }
+            }
+            catch {
+                print(error)
+            }
+        }
+        task.resume()
+    }
 }
