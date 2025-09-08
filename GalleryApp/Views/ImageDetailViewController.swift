f0f70a9cedd73fdb8c17aed197438a1ef764362a
Daria Gavrilova <dashkagavrilova024@gmail.com>
2025-09-08 05:14:21 -0700

    Added Image Detail View Controller CocoaTouchClass

---
+++ GalleryApp/Views/ImageDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ImageDetailViewController.swift
+//  GalleryApp
+//
+//  Created by MAC on 08.09.25.
+//
+
+import UIKit
+
+class ImageDetailViewController: UIViewController {
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var desctiptionText: UILabel!
+    @IBOutlet weak var username: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
