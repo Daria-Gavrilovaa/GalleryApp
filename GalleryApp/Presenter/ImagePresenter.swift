2f722fd27c76128465dcb6b325ade35e6d6667c7
Daria Gavrilova <dashkagavrilova024@gmail.com>
2025-09-08 11:14:53 -0700

    Fixed one feature

--- GalleryApp/Presenter/ImagePresenter.swift
+++ GalleryApp/Presenter/ImagePresenter.swift
@@ -41,7 +41,7 @@ class ImagePresenter {
                 
             case .failure(let networkError):
                 print("\(networkError)")
-                
+                self.isPageRefreshing = false
             }
         }
     }
